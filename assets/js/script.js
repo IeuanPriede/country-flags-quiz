f4c9dc1c32c25ec31097b5640f7cd48669b541fd
Ieuan Priede <priedeieuan@gmail.com>
2024-11-27 07:53:50 +0000

    Add eventListener and function to start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
+const startButton = document.getElementById('start-btn');
+
+startButton.addEventListener('click', startGame);
+
 function startGame() {
-    
+    console.log('Started');
+    startButton.classList.add('hide');
 }
 
 function selectNextQuestion() {
