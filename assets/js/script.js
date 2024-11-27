58e28ad5b356db7a75a32759e8265c46eb43d516
Ieuan Priede <priedeieuan@gmail.com>
2024-11-27 16:18:44 +0000

    Add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ const questionAreaElement = document.getElementById('question-area');
 const answerButtonsElement = document.getElementById('answer-buttons');
 const question1Element = document.getElementById('question1');
 
-
-
+/* Listens for a mouse click to start game */
 startButton.addEventListener('click', startGame);
 
+/* Starts the game */
 function startGame() {
     console.log('Started');
     startButton.classList.add('hide');
@@ -17,10 +17,12 @@ function startGame() {
     setNextQuestion();
 }
 
+/* Checks for correct/wrong answers */
 function checkAnswer() {
 
 }
 
+/* Counts score */
 function incrementScore() {
 
 }
@@ -28,4 +30,3 @@ function incrementScore() {
 function gameArea() {
 
 }
-
