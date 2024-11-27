2293108f638540c257f25df2127033b51de9bef8
Ieuan Priede <priedeieuan@gmail.com>
2024-11-27 15:17:45 +0000

    Restarting javescript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,4 @@
 const startButton = document.getElementById('start-btn');
-const nextButton = document.getElementById('next-btn');
-const gameAreaElement = document.getElementById('game-area');
-const questionElement = document.getElementById(question);
-const answerButtonsElement = document.getElementById('answer-buttons');
-
-let currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
 
@@ -15,32 +9,14 @@ function startGame() {
     setNextQuestion();
 }
 
-function setNextQuestion() {
-    showQuestion(currentQuestionIndex)
-}
-
 function showQuestion(question) {
-}
-
-function selectAnswer() {
 
 }
 
-const questions = [
-    {
-        question: "assets/images/Flag_of_Latvia.svg.png",
-        answers: [
-            { text: 'Latvia', correct: true},
-            { text: 'Austria', correct: false},
-            { text: 'Armenia', correct: false},
-            { text: 'France', correct: false}
-        ]
-        question: "assets/images/Flag_of_Ghana.svg.png",
-        answers: [
-            { text: 'India', correct: false},
-            { text: 'Cuba', correct: false},
-            { text: 'Ghana', correct: true},
-            { text: 'Sierra Leone', correct: false}
-        ]
-    }
-]
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
