3d14c3e04ab114b32057c00d99d5ca109db98b5b
Ieuan Priede <priedeieuan@gmail.com>
2024-11-27 16:05:09 +0000

    Add question1 Element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const gameAreaElement = document.getElementById('game-area');
+const questionAreaElement = document.getElementById('question-area');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const question1Element = document.getElementById('question1');
+
+let currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
 
@@ -6,11 +13,16 @@ function startGame() {
     console.log('Started');
     startButton.classList.add('hide');
     gameAreaElement.classList.remove('hide');
+    question1Element.classList.remove('hide');
     setNextQuestion();
 }
 
-function showQuestion(question) {
+function setNextQuestion() {
+    showQuestion(currentQuestionIndex)
+}
 
+function showQuestion(questionArea) {
+    questionElement.src= question.question
 }
 
 function checkAnswer() {
@@ -20,3 +32,19 @@ function checkAnswer() {
 function incrementScore() {
 
 }
+
+function gameArea() {
+
+}
+
+const questions = [
+     {
+      question: '',
+      answers: [
+        { text: 'Latvia', correct: true },
+        { text: 'Austria', correct: false },
+        { text: 'Armenia', correct: false},
+        { text: 'France', correct: false}
+      ]
+    },
+]
