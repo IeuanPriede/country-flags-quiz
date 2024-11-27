58b67c2832e3abe44dafea6485b40013cd39a98f
Ieuan Priede <priedeieuan@gmail.com>
2024-11-27 08:52:45 +0000

    Start button funstions properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 const startButton = document.getElementById('start-btn');
+const gameAreaElement = document.getElementById('game-area');
 
 startButton.addEventListener('click', startGame);
 
 function startGame() {
     console.log('Started');
     startButton.classList.add('hide');
+    gameAreaElement.classList.remove('hide');
 }
 
 function selectNextQuestion() {
